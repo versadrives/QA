d18922d08d858a3ed6177a8dda72b81ed17f61bf
versadrives <engrg10765@versadrives.com>
2025-08-26 12:27:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(
     static_folder=resource_path("static"),
 )
 
-socketio = SocketIO(app, async_mode="threading", cors_allowed_origins="*")
+socketio = SocketIO(app,cors_allowed_origins="*")
 
 DB_FILE = "scan_log.db"
 

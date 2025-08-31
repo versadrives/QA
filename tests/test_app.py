cd81d2089b37dfac589b1681e630ca8a227ddf2c
versadrives <engrg10765@versadrives.com>
2025-08-31 15:39:06 +0530

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    rv = client.get('/')
+    assert rv.status_code == 200
+    assert b'scans' in rv.data
+
+def test_export_requires_dates(client):
+    rv = client.get('/export')
+    assert rv.status_code == 400

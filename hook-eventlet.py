5cc770295b4050e5010d9a4b9025a5b7fa68dc05
versadrives <engrg10765@versadrives.com>
2025-08-31 17:01:23 +0530

    Create hook-eventlet.py

---
+++ hook-eventlet.py
@@ -0,0 +1,11 @@
+hiddenimports = [
+    'eventlet.hubs.epolls',
+    'eventlet.hubs.kqueue',
+    'eventlet.hubs.poll',
+    'eventlet.hubs.selects',
+    'dns.rdtypes.ANY',
+    'dns.rdtypes.IN',
+    'dns.rdtypes.MAIL',
+    'dns.rdtypes.RP',
+    'dns.rdtypes.OTHER'
+]

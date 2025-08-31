79dd57586cfc8f38513b856d5e093173654de934
versadrives <engrg10765@versadrives.com>
2025-08-31 16:52:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import eventlet
 eventlet.monkey_patch()
-from app import app
-
 from flask import Flask, render_template, request, redirect, send_file, jsonify
 from flask_socketio import SocketIO
 import sqlite3
